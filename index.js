86dce2710387ef552fa197f07af0eb385ff06015
github username <github email address>
2019-07-17 17:03:06 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', runJavascript)
+
+function runJavascript() {
+    const text = document.querySelector('#text')
+    text.innerHTML = "This is really cool!"
+}
